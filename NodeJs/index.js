e2f2d6cc53eec0cfe6206d102d3beb8e81514741
Harshaljain04 <harshaljn04@gmail.com>
2024-09-04 13:30:53 +0530

    rename

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -2,4 +2,10 @@
 
 const fs = require('fs');
 
-fs.writeFileSync('read.txt','Welcome to Techno NJR');
+fs.writeFileSync('read.txt','Welcome to Techno NJR ');
+
+fs.appendFileSync('read.txt','Hello Tr4 ');
+fs.appendFileSync('read.txt','Welcome Back');
+// let data= fs.readFileSync('read.txt');
+// console.log(data.toString());
+fs.renameSync('read.txt','readwrite.txt');
