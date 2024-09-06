1d0eb0419db46e69524183ab4d9193147cd798a2
Harshaljain04 <harshaljn04@gmail.com>
2024-09-06 13:59:18 +0530

    events

--- Expressjs/index.js
+++ Expressjs/index.js
@@ -3,38 +3,58 @@ const http = require("http")
 const event = new EventEmitter()
 
 
+
+
 const server = http.createServer((req,res)=>{
     if(req.url == "/"){
         res.end("Home page")
-        event.emit("HomePageAccessed")
+        // event.emit("HomePageAccessed")
     }
     else if(req.url == "/about"){
         res.end("About page")
-        event.emit("AboutPageAccessed")
+        // event.emit("AboutPageAccessed")
     }
     else if(req.url == "/contact"){
         res.end("Contact page")
-        event.emit("ContactPageAccessed")
+        // event.emit("ContactPageAccessed")
     }
     else{
         res.end("Unknown Route");
-        event.emit("UnknownPageAccessed", req.url);
+        // event.emit("UnknownPageAccessed", req.url);
     }   
 });
 
-event.on("HomePageAccessed",()=>{
-    console.log("Home page accessed")
+// server.on("request",(req,res)=>{
+//     if(req.url != "/favicon.ico"){
+//         console.log("Request event",req.url)
+//     }
+// });
+
+// server.on("close",()=>{
+//     console.log("Server closed")
+// });
+
+// setTimeout(()=>{
+//     server.close()
+// },5000);
+
+server.on("connection",()=>{
+    console.log("connected")
 });
-event.on("AboutPageAccessed",()=>{
-    console.log("About page accessed")
-});
-event.on("ContactPageAccessed",()=>{
-    console.log("Contact page accessed")
-});
-event.on("UnknownPageAccessed",(url)=>{
-    if(url != "/favicon.ico"){
-    console.log("Unknown page accessed",url)
-    }
-});
+
+// event.on("HomePageAccessed",()=>{
+//     console.log("Home page accessed")
+// });
+// event.on("AboutPageAccessed",()=>{
+//     console.log("About page accessed")
+// });
+// event.on("ContactPageAccessed",()=>{
+//     console.log("Contact page accessed")
+// });
+// event.on("UnknownPageAccessed",(url)=>{
+//     if(url != "/favicon.ico"){
+//     console.log("Unknown page accessed",url)
+//     }
+// });
 
 server.listen(3000,"127.0.0.1")
